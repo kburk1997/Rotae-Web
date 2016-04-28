bedfd107246f6d6bcdb51175c68c19c73829e8c0
kburk1997 <kburk97@optonline.net>
2016-04-28 17:23:19 -0400

    setup rails environment

---
+++ Rotae/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rotae::Application.config.secret_key_base = 'aaf1b033ac25bbd6aff601b8655607daf03a7904d8ee14caba29de2fde6d8cb018625b4fab567f52685c666df697df984d26c337ea946f4f2f33148e97281db2'
---
+++ Rotae/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rotae::Application.config.session_store :cookie_store, key: '_Rotae_session'
